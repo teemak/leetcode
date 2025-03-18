6a15305aee8de1871fc944033b8c2e82a1267740
Tee Mak <9258270+teemak@users.noreply.github.com>
2025-03-17 22:16:30 -0400

    Time: 4 ms (99.38%), Space: 17.7 MB (78.32%) - LeetHub

--- 3768-check-if-digits-are-equal-in-string-after-operations-i/3768-check-if-digits-are-equal-in-string-after-operations-i.py
+++ 3768-check-if-digits-are-equal-in-string-after-operations-i/3768-check-if-digits-are-equal-in-string-after-operations-i.py
@@ -3,11 +3,11 @@ class Solution:
         digits = list(map(int, s))
         n = len(digits) - 1
         l, r = digits[0], digits[1]
-        multiplier = 1
+        factor = 1
 
         for i in range(1, n):
-            multiplier = (multiplier * (n - i)) // i
-            mod_mult = multiplier % 10
+            factor = (factor * (n - i)) // i
+            mod_mult = factor % 10
 
             l = (l + digits[i] * mod_mult) % 10
             r = (r + digits[i + 1] * mod_mult) % 10
