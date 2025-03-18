b63c3655eddedeab8e3ce23f0ece4fe1300bff6e
Tee Mak <9258270+teemak@users.noreply.github.com>
2025-03-17 22:53:05 -0400

    Time: 4 ms (64.86%), Space: 17.8 MB (40.52%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.py
+++ 0013-roman-to-integer/0013-roman-to-integer.py
@@ -2,26 +2,18 @@ class Solution:
     def romanToInt(self, s: str) -> int:
         numerals = {
             'I': 1,
-            'IV': 4,
             'V': 5,
-            'IX': 9,
             'X': 10,
-            'XL': 40,
             'L': 50,
-            'XC': 90,
             'C': 100,
-            'CD': 400,
             'D': 500,
-            'CM': 900,
             'M': 1000,
         }
-        total, i = 0, 0
+        total = numerals.get(s[-1])
 
-        while i < len(s):
-            if i < len(s) - 1 and s[i: i + 2] in numerals:
-                total += numerals[s[i:i+2]]
-                i += 2
+        for i in reversed(range(len(s) - 1)):
+            if numerals[s[i]] < numerals[s[i + 1]]:
+                total -= numerals[s[i]]
             else:
                 total += numerals[s[i]]
-                i += 1
         return total
