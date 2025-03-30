ac1cd7bbb4a14992e5a5bbad1f0d729f4bc98be0
Tee Mak <9258270+teemak@users.noreply.github.com>
2025-03-30 01:06:02 -0400

    Time: 13 ms (25.35%), Space: 18.3 MB (49.09%) - LeetHub

--- 1108-analyze-user-website-visit-pattern/1108-analyze-user-website-visit-pattern.py
+++ 1108-analyze-user-website-visit-pattern/1108-analyze-user-website-visit-pattern.py
@@ -1,3 +1,4 @@
+# Excellent explanation: https://leetcode.com/problems/analyze-user-website-visit-pattern/solutions/899805/detailed-easy-to-follow-python-solution-clearly-explained/?envType=company&envId=amazon&favoriteSlug=amazon-thirty-days
 class Solution:
     def mostVisitedPattern(self, username: List[str], timestamp: List[int], website: List[str]) -> List[str]:
         users = defaultdict(list)
