d275280800a31fceea445a2e2f9cb39a25f740a5
Tee Mak <9258270+teemak@users.noreply.github.com>
2025-03-29 21:22:09 -0400

    Time: 7 ms (100%), Space: 17.9 MB (0%) - LeetHub

--- 3811-reverse-degree-of-a-string/3811-reverse-degree-of-a-string.py
+++ 3811-reverse-degree-of-a-string/3811-reverse-degree-of-a-string.py
@@ -1,10 +1,9 @@
 class Solution:
     def reverseDegree(self, s: str) -> int:
         total = 0
-        z = ord('z')
 
         for i, char in enumerate(s, 1):
-            total += (z - ord(char) + 1) * i
+            total += (ord('z') - ord(char) + 1) * i
             
         return total
         
