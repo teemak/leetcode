e6982a860b7bb607a53b0e8d6ecf8988956cfe2e
Tee Mak <9258270+teemak@users.noreply.github.com>
2025-04-01 19:35:49 -0400

    Time: 0 ms (100%), Space: 17.7 MB (52.26%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.py
+++ 0069-sqrtx/0069-sqrtx.py
@@ -1,19 +1,25 @@
 class Solution:
     def mySqrt(self, x: int) -> int:
+        # need this for numbers smaller than 2 i.e. 1 is 1, 0 is 0
         if x < 2:
             return x
 
+        # should start at 1 because of x = 2, half of 2 is 1 making it the hi
+        # when x = 2 the result should be 1
         lo, hi = 1, x // 2
 
+        # x = 8 [1,2,3,4]
+        #        l   m h
+        #        l m h
+
+        # must iterate while lo is less than or equal to hi
+        # i.e. 6 should return 2
         while lo <= hi:
             mid = (hi + lo) // 2
-            num = mid * mid
-
-            if num > x:
+            if mid * mid > x:
                 hi = mid - 1
-            elif num < x:
-                lo = mid + 1
             else:
-                return mid
+                lo = mid + 1
 
+        # need to return hi for overshoots
         return hi
