76a3c4cd875fe8abd09cffb67b54013057a0730a
Tee Mak <9258270+teemak@users.noreply.github.com>
2025-04-01 17:48:56 -0400

    Time: 177 ms (28.02%), Space: 18.9 MB (76.21%) - LeetHub

--- 0907-koko-eating-bananas/0907-koko-eating-bananas.py
+++ 0907-koko-eating-bananas/0907-koko-eating-bananas.py
@@ -1,28 +1,18 @@
 class Solution:
     def minEatingSpeed(self, piles: List[int], h: int) -> int:
-        pq = []
-        min_k, max_k = 1, max(piles)
+        def condition(k):
+            time = 0
+            for pile in piles:
+                hours = math.ceil(pile/k)
+                time += hours
+            return time <= h
 
-        for p in piles:
-            heapq.heappush(pq, -p)
+        left, right = 1, max(piles)
 
-        def time(k):
-            t = 0
-            for p in piles:
-                t += math.ceil(p/k)
-            return t
-
-        while min_k < max_k:
-            k = (min_k + max_k) // 2
-            t = time(k)
-            if t <= h:
-                max_k = k
+        while left < right:
+            mid = (right + left) // 2
+            if condition(mid):
+                right = mid
             else:
-                min_k = k + 1
-
-        return min_k
-
-
-        return -pq[0][0]
-
-
+                left = mid + 1
+        return left
