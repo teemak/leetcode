937f733341f977fc23f3f9829e031b2cedd3b64e
Tee Mak <9258270+teemak@users.noreply.github.com>
2025-04-02 13:31:32 -0400

    Time: 3 ms (98.96%), Space: 17.9 MB (64.67%) - LeetHub

--- 0079-word-search/0079-word-search.py
+++ 0079-word-search/0079-word-search.py
@@ -1,30 +1,39 @@
 class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
-        rows, cols = len(board), len(board[0])
-        directions = [(0,1),(0,-1),(1,0),(-1,0)]
+        if not board or not word:
+            return False
 
-        def dfs(row, col, index):
-            # happy case
-            if index == len(word):
-                return True
-            # base case - out of bounds
-            if row < 0 or row >= rows or col < 0 or col >= cols or board[row][col] != word[index]:
+        rows, cols = len(board), len(board[0])
+        n = len(word)
+        if n > rows * cols:
+            return False
+        cell_counts = Counter(cell for row in board for cell in row)
+        char_counts = Counter(word)
+        for char, count in char_counts.items():
+            if cell_counts[char] < count:
                 return False
-            # mark cell as visited
-            temp, board[row][col] = board[row][col], '#'
-            # recursive case
+        
+        if char_counts[word[-1]] < char_counts[word[0]]:
+            word = word[::-1]
+        
+        directions = [(1,0), (-1,0), (0,1), (0,-1)]
+
+        def dfs(row, col, index, visited):
+            if index == n:
+                return True
+            if row < 0 or row >= rows or col < 0 or col >= cols or (row, col) in visited or board[row][col] != word[index]:
+                return False
+            visited.add((row, col))
             for dx, dy in directions:
                 x, y = dx + row, dy + col
-                if dfs(x, y, index + 1):
+                if dfs(x, y, index + 1, visited):
                     return True
-            # backtrack
-            board[row][col] = temp
-
+            visited.remove((row, col))
             return False
 
         for row in range(rows):
             for col in range(cols):
-                if dfs(row, col, 0):
+                if dfs(row, col, 0, set()):
                     return True
         
         return False
