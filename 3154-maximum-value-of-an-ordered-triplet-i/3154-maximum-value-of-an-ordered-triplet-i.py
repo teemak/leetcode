bb7c18379654e50c4cd46f2d5189349561d0f10c
Tee Mak <9258270+teemak@users.noreply.github.com>
2025-04-01 21:44:49 -0400

    Time: 1 ms (84.04%), Space: 17.9 MB (19.2%) - LeetHub

--- 3154-maximum-value-of-an-ordered-triplet-i/3154-maximum-value-of-an-ordered-triplet-i.py
+++ 3154-maximum-value-of-an-ordered-triplet-i/3154-maximum-value-of-an-ordered-triplet-i.py
@@ -1,55 +1,11 @@
-'''
-in:  [12,6,1,2,7]
-
-(0,2,4) -> (12 - 1 * 7)
-
-out: 77
-
-max value over (i,j,k) i < j < k indices
-
-return 0 else max(i, j, k)
-
-constraints:
-1. sequence matters
-2. i - j * k (order of operations matter), greedy won't work
-3. input length will always be valid up to 100
-4. values are 1 to 10_000_000
-
-plan:                                                       time          space
-sliding window
-1. try to get the largest difference between i - j          O(n)          O(1)
-    - 2 (i)passes, get the min, (ii)get product of diff
-
-steps:                  step
- i
-[12, 6, 1, 2, 7]        6    * [1, 12, 42]
-     j                  11   *    [22, 77]
-                        10   *    [70]
-                        5 (not valid)
-
-** need to iterate i to find largest diff                    
-   i    j
-[1,10,3,4,19]           7    * [28, 133]
-                        6
-                        
-will the largest difference always contain the largest product?
-j can only iterate up to last element
-
-implementation:
-1. iterate i
-2. iterate j, where j is i + 1 up to len(input) - 1
-3. iterate k, where k is j + 1 up to len(input)
-
-'''
 class Solution:
     def maximumTripletValue(self, nums: List[int]) -> int:
         n = len(nums)
-        result = 0
-        for k in range(2, n):
-            prefix = nums[0]
-            for j in range(1, k):
-                result = max(result, (prefix - nums[j]) * nums[k])
-                prefix = max(prefix, nums[j])
+        res = imax = dmax = 0
 
-        return result
+        for k in range(n):
+            res = max(res, dmax * nums[k])
+            dmax = max(dmax, imax - nums[k])
+            imax = max(imax, nums[k])
         
+        return res
