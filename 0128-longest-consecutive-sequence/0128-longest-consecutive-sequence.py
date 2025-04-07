f196fdd09a695efd6f55ddaacd7e93538b459b5d
Tee Mak <9258270+teemak@users.noreply.github.com>
2025-04-07 12:57:05 -0400

    Time: 60 ms (33.65%), Space: 33.1 MB (89.7%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.py
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.py
@@ -1,40 +1,17 @@
-'''
-input: nums = [100, 4, 200, 1, 3, 2]
-                    1234 => 4
-                    longest consecutive subsequence
-
-                [0,3,7,2,5,8,4,6,0,1]
-                 012345678 => 9
-output: 4
-
-1. sort list
-2. iterate pointer while updating global counter
-3. must be O(n) because we need to check every element
-
-constraints:
-duplicate numbers - 
-can use a set or condition check
-need to work backwards so there is no index out of range
-
-'''
 class Solution:
     def longestConsecutive(self, nums: List[int]) -> int:
         if not nums:
             return 0
-        result = 0
+            
+        longest = 0
         num_set = set(nums)
 
         for num in num_set:
             if num - 1 not in num_set:
-                current_num = num
-                current_consecutive = 1
+                length = 1
 
-                while current_num + 1 in num_set:
-                    current_num += 1
-                    current_consecutive += 1
-                result = max(current_consecutive, result)
+                while num + length in num_set:
+                    length += 1
+                longest = max(longest, length)
 
-        return result
-
-            
-        
+        return longest
