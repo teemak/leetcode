7e0149841e1d0745e6d04e1dfa7d15adfb95a9cc
Tee Mak <9258270+teemak@users.noreply.github.com>
2025-04-07 20:48:15 -0400

    Time: 4 ms (49.03%), Space: 18.5 MB (92.35%) - LeetHub

--- 0268-missing-number/0268-missing-number.py
+++ 0268-missing-number/0268-missing-number.py
@@ -1,24 +1,6 @@
-'''
-input. 
- nums = [3, 0, 1]
-
-
-output. 
-2
-
-
-plan - create a set of nums and remove as we iterate, return last element in set
-constraints:
-
-
-'''
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
-        n = len(nums)
-        seen = set(range(n + 1))
-
-        for num in nums:
-            if num in seen:
-                seen.remove(num)
-                
-        return seen.pop()
+        missing = len(nums)
+        for i, num in enumerate(nums):
+            missing ^= i ^ num
+        return missing
