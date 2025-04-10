cc10456b05db0f6ac95b6a12b238e5f2d3269ff3
Tee Mak <9258270+teemak@users.noreply.github.com>
2025-04-09 20:22:58 -0400

    Time: 0 ms (100%), Space: 17.7 MB (99.04%) - LeetHub

--- 3243-count-the-number-of-powerful-integers/3243-count-the-number-of-powerful-integers.py
+++ 3243-count-the-number-of-powerful-integers/3243-count-the-number-of-powerful-integers.py
@@ -1,25 +1,26 @@
 class Solution:
     def numberOfPowerfulInt(self, start: int, finish: int, limit: int, s: str) -> int:
-        low = str(start)
-        high = str(finish)
-        n = len(high)
-        low = low.zfill(n)
-        pre_len = n - len(s)
+        start_ = str(start - 1)
+        finish_ = str(finish)
+        return self.calculate(finish_, s, limit) - self.calculate(start_, s, limit)
 
-        @cache
-        def dfs(i, limit_low, limit_high):
-            if i == n:
-                return 1
-            lo = int(low[i]) if limit_low else 0
-            hi = int(high[i]) if limit_high else 9
-            res = 0
-            if i < pre_len:
-                for digit in range(lo, min(hi, limit) + 1):
-                    res += dfs(i + 1, limit_low and digit == lo, limit_high and digit == hi)
-            else:
-                x = int(s[i - pre_len])
-                if lo <= x <= min(hi, limit):
-                    res = dfs(i + 1, limit_low and x == lo, limit_high and x == hi)
-            return res
+    def calculate(self, x, s, limit):
+        if len(x) < len(s):
+            return 0
+        if len(x) == len(s):
+            return 1 if x >= s else 0
 
-        return dfs(0, True, True)
+        suffix = x[len(x) - len(s) :]
+        count = 0
+        pre_len = len(x) - len(s)
+
+        for i in range(pre_len):
+            if limit < int(x[i]):
+                count += (limit + 1) ** (pre_len - i)
+                return count
+            count += int(x[i]) * (limit + 1) ** (pre_len - 1 - i)
+
+        if suffix >= s:
+            count += 1
+
+        return count
