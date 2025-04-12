76634a87c0d64f930607c86135fa7b0f99673af8
Tee Mak <9258270+teemak@users.noreply.github.com>
2025-04-12 17:06:20 -0400

    Time: 0 ms (100%), Space: 17.7 MB (91.49%) - LeetHub

--- 2561-number-of-distinct-averages/2561-number-of-distinct-averages.py
+++ 2561-number-of-distinct-averages/2561-number-of-distinct-averages.py
@@ -1,5 +1,32 @@
+'''
+in______
+[4,1,4,0,3,5]
+       x   x
+
+
+--------
+out_____
+2
+
+solutions:
+1. two pointers
+
+plan:
+1. sort the list, ascending order
+2. declare 2 pointers
+3. form pairs
+4. compute avg
+5. move pointers toward middle
+'''
 class Solution:
     def distinctAverages(self, nums: List[int]) -> int:
-       a, n = sorted(nums), len(nums) // 2
-       res = len({a[i] + a[~i] for i in range(n)}) 
-       return res
+        res = set()
+        nums = sorted(nums)
+        left, right = 0, len(nums) - 1
+
+        while left < right:
+            res.add((nums[left] + nums[right]) / 2)
+            left += 1
+            right -= 1
+        
+        return len(res)
