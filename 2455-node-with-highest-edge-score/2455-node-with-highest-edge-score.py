4421db7e0ab0cb9ebe8a395a7a4480b7ff992912
Tee Mak <9258270+teemak@users.noreply.github.com>
2025-04-13 16:26:36 -0400

    Time: 75 ms (99.19%), Space: 29.4 MB (86.69%) - LeetHub

--- 2455-node-with-highest-edge-score/2455-node-with-highest-edge-score.py
+++ 2455-node-with-highest-edge-score/2455-node-with-highest-edge-score.py
@@ -1,15 +1,8 @@
 class Solution:
     def edgeScore(self, edges: List[int]) -> int:
-        score = defaultdict(int)
-        max_score = result_node = -1
+        scores = [0] * len(edges)
 
-        for i, dest in enumerate(edges):
-            score[dest] += i
+        for i, edge in enumerate(edges):
+            scores[edge] += i
 
-        for node in score:
-            valid = score[node] == max_score and node < result_node
-            if score[node] > max_score or valid:
-                max_score = score[node]
-                result_node = node
-        
-        return result_node
+        return scores.index(max(scores))
