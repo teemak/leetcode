08d081f4a31c1acff219c153f9faa5f4b7de6161
Tee Mak <9258270+teemak@users.noreply.github.com>
2025-04-13 11:19:40 -0400

    Time: 630 ms (35.81%), Space: 20.7 MB (69.26%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -1,23 +1,58 @@
+'''
+in
+  i
+[-1, 0, 1, 2, -1, -4]
+     l             r
+
+
+out
+[ [-1, -1, 2], [-1, 0, 1], ]
+
+list of lists
+indices store values of elements that sum to zero
+
+plan
+1. sort
+2. use two pointers
+3. left will always be i + 1
+4. iterate over each i
+5. for each i, use two pointers
+6. check the sum of i + left + right, store indices in results
+7. move pointers - 
+    i. left moves forward when sum needs to increase
+    ii. right moves towards middle when sum needs to decrease
+8. store the values in global result, not indices
+
+reflections:
+1. did not think of duplicates
+2. stored the indices; need to make sure I express the output data type
+3. mistake on sorting the nums list, used sorted(nums) but that does nothing unless I store in variable, nums.sort() sorts in place
+
+'''
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-        res = []
+        triplets = []
         nums.sort()
+        n = len(nums)
 
-        def helper(nums, i, res):
-            seen = set()
-            j = i + 1
-            while j < len(nums):
-                diff = -nums[i] - nums[j]
-                if diff in seen:
-                    res.append([nums[i], nums[j], diff])
-                    while j + 1 < len(nums) and nums[j] == nums[j + 1]:
-                        j += 1
-                seen.add(nums[j])
-                j += 1
+        for i in range(0, n - 2):
+            # need to skip duplicates as expressed in description
+            if i > 0 and nums[i] == nums[i - 1]:
+                continue 
 
-        for i in range(len(nums)):
-            if nums[i] > 0:
-                break
-            if i == 0 or nums[i - 1] != nums[i]:
-                helper(nums, i, res)
-        return res
+            left, right = i + 1, n - 1
+            while left < right:
+                total = nums[i] + nums[left] + nums[right]
+                if total == 0:
+                    triplets.append([nums[i], nums[left], nums[right]])
+                    left += 1
+                    right -= 1
+                    while left < right and nums[left] == nums[left - 1]:
+                        left += 1
+                    while left < right and nums[right] == nums[right + 1]:
+                        right -= 1
+                elif total < 0:
+                    left += 1
+                elif total > 0:
+                    right -= 1
+        return triplets
