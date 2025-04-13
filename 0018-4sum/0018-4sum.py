09ff709662139a1540538baeafabd5eb4c9228cb
Tee Mak <9258270+teemak@users.noreply.github.com>
2025-04-13 13:31:41 -0400

    Time: 18 ms (95.33%), Space: 18.2 MB (7.47%) - LeetHub

--- 0018-4sum/0018-4sum.py
+++ 0018-4sum/0018-4sum.py
@@ -1,43 +1,39 @@
-'''
-in [1, 0, -1, 0, -2, 2]
-
-out
-[
-    [-2, -1, 1, 2],
-    [-2,  0, 0, 2],
-    [-1,  0, 0, 1],
-]
-
-return => list of values that sum to target
-1. store the values and index in a dict
-2. iterate the nums list
-3. for every pair, use two pointers that will calculate the sum
-4. if the sum is equal to the target - add the values to the dict
-
-plan:
-sort the nums list
-store the values in a set
-iterate and store the set
-
- i  i   i  
-[1, 0, -1, 0, -2, 2]
-           j   
-               k  k
-'''
 class Solution:
     def fourSum(self, nums: List[int], target: int) -> List[List[int]]:
-        nums.sort()
-        n = len(nums)
-        result = set()
+        def k_sum(nums, target, k):
+            res = []
+            if not nums:
+                return res
+            
+            avg_val = target // k
 
-        for i in range(n - 3):
-            for j in range(i + 1, n - 2):
-                # must be reset for each new pair
-                seen = set()
-                for k in range(j + 1, n):
-                    diff = target - nums[i] - nums[j] - nums[k]
-                    if diff in seen:
-                        result.add((nums[i], nums[j], nums[k], diff))
-                    seen.add(nums[k])
-    
-        return [list(quads) for quads in result]
+            if avg_val < nums[0] or nums[-1] < avg_val:
+                return res
+            
+            if k == 2:
+                return two_sum(nums, target)
+            
+            for i in range(len(nums)):
+                if i == 0 or nums[i - 1] != nums[i]:
+                    for subset in k_sum(nums[i + 1:], target - nums[i], k - 1):
+                        res.append([nums[i]] + subset)
+            return res
+
+        def two_sum(nums, target):
+            res = []
+            lo, hi = 0, len(nums) - 1
+
+            while lo < hi:
+                curr_sum = nums[lo] + nums[hi]
+                if curr_sum < target or (lo > 0 and nums[lo] == nums[lo - 1]):
+                    lo += 1
+                elif curr_sum > target or (hi < len(nums) - 1 and nums[hi] == nums[hi + 1]):
+                    hi -= 1
+                else:
+                    res.append([nums[lo], nums[hi]])
+                    lo += 1
+                    hi -= 1
+            return res
+        
+        nums.sort()
+        return k_sum(nums, target, 4)
