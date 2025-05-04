409aab71bf7bce5250f2f4971a32b33db988ca86
Tee Mak <9258270+teemak@users.noreply.github.com>
2025-05-04 14:54:27 -0400

    Time: 93 ms (28.65%), Space: 18.9 MB (92.3%) - LeetHub

--- 0227-basic-calculator-ii/0227-basic-calculator-ii.py
+++ 0227-basic-calculator-ii/0227-basic-calculator-ii.py
@@ -1,34 +1,32 @@
 class Solution:
     def calculate(self, s: str) -> int:
-        s = s.replace(' ', '')
         n = len(s)
-        num = 0
-        last_op = '+'
-        result = 0
-        tail = 0
-        i = 0
+        operator = '+'
+        num = result = tail = i = 0
 
         while i < n:
-            ch = s[i]
-            if ch.isdigit():
+            element = s[i]
+            if element.isdigit():
                 num = 0
                 while i < n and s[i].isdigit():
                     num = num * 10 + int(s[i])
                     i += 1
-                i -= 1
-            if not ch.isdigit() or i == n - 1:
-                if last_op == '+':
-                    result += num
-                    tail = num
-                elif last_op == '-':
-                    result -= num
-                    tail = -num
-                elif last_op == '*':
-                    result = result - tail + (tail * num)
-                    tail = tail * num
-                elif last_op == '/':
-                    result = result - tail + int(tail / num)
-                    tail = int(tail / num)
-                last_op = ch
+                match operator:
+                    case '+':
+                        result += num
+                        tail = num
+                    case '-':
+                        result -= num
+                        tail = -num
+                    case '*':
+                        result = result - tail + (tail * num)
+                        tail = tail * num
+                    case '/':
+                        result = result - tail + int(tail / num)
+                        tail = int(tail / num)
+                num = 0
+                continue
+            elif element in '+-*/':
+                operator = element
             i += 1
         return result
