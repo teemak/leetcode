9a0757b2e308da3d7cfbe849f59a77f9a7878ee6
Tee Mak <9258270+teemak@users.noreply.github.com>
2025-05-04 14:08:53 -0400

    Time: 95 ms (28.06%), Space: 18.8 MB (92.3%) - LeetHub

--- 0227-basic-calculator-ii/0227-basic-calculator-ii.py
+++ 0227-basic-calculator-ii/0227-basic-calculator-ii.py
@@ -1,26 +1,34 @@
 class Solution:
     def calculate(self, s: str) -> int:
-        def helper(s, i):
-            num = 0
-            stack = []
-            sign = '+'
+        s = s.replace(' ', '')
+        n = len(s)
+        num = 0
+        last_op = '+'
+        result = 0
+        tail = 0
+        i = 0
 
-            while i < len(s):
-                ch = s[i]
-                if ch.isdigit():
-                    num = num * 10 + int(ch)
-
-                if ch in '+-*/' or i == len(s) - 1:
-                    if sign == '+':
-                        stack.append(num)
-                    elif sign == '-':
-                        stack.append(-num)
-                    elif sign == '*':
-                        stack[-1] *= num
-                    elif sign == '/':
-                        stack[-1] = int(stack[-1] / num)
-                    sign = ch
-                    num = 0
-                i += 1
-            return sum(stack)
-        return helper(s.replace(' ', ''), 0)
+        while i < n:
+            ch = s[i]
+            if ch.isdigit():
+                num = 0
+                while i < n and s[i].isdigit():
+                    num = num * 10 + int(s[i])
+                    i += 1
+                i -= 1
+            if not ch.isdigit() or i == n - 1:
+                if last_op == '+':
+                    result += num
+                    tail = num
+                elif last_op == '-':
+                    result -= num
+                    tail = -num
+                elif last_op == '*':
+                    result = result - tail + (tail * num)
+                    tail = tail * num
+                elif last_op == '/':
+                    result = result - tail + int(tail / num)
+                    tail = int(tail / num)
+                last_op = ch
+            i += 1
+        return result
