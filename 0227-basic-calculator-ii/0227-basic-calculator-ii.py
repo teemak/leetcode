f99dd428aa398ca8c68e10e2b7ceed717dc71fae
Tee Mak <9258270+teemak@users.noreply.github.com>
2025-05-04 13:35:37 -0400

    Time: 143 ms (8.59%), Space: 18.9 MB (79.5%) - LeetHub

--- 0227-basic-calculator-ii/0227-basic-calculator-ii.py
+++ 0227-basic-calculator-ii/0227-basic-calculator-ii.py
@@ -1,25 +1,42 @@
 class Solution:
     def calculate(self, s: str) -> int:
-        stack = []
-        current_num = 0
-        operator = '+'
+        def precedence(op):
+            if op in '*/':
+                return 2
+            if op in '+-':
+                return 1
+            return 0
+        def apply_op(op, b, a):
+            if op == '+': return a + b
+            if op == '-': return a - b
+            if op == '*': return a * b
+            if op == '/': return int(a // b)
+        nums = []
+        ops = []
+        i = 0
         n = len(s)
 
-        for i, ch in enumerate(s):
-            if ch.isdigit():
-                current_num = current_num * 10 + int(ch)
-            
-            if ch in '+-*/' or i == n - 1:
-                if operator == '+':
-                    stack.append(current_num)
-                elif operator == '-':
-                    stack.append(-current_num)
-                elif operator == '*':
-                    stack[-1] = stack[-1] * current_num
-                elif operator == '/':
-                    stack[-1] = int(stack[-1] / current_num)
-
-                operator = ch
-                current_num = 0
-
-        return sum(stack) 
+        while i < n:
+            if s[i] == ' ':
+                i += 1
+                continue
+            if s[i].isdigit():
+                num = 0
+                while i < n and s[i].isdigit():
+                    num = num * 10 + int(s[i])
+                    i += 1
+                nums.append(num)
+            else:
+                while ops and precedence(ops[-1]) >= precedence(s[i]):
+                    op = ops.pop()
+                    b = nums.pop()
+                    a = nums.pop()
+                    nums.append(apply_op(op, b, a))
+                ops.append(s[i])
+                i += 1
+        while ops:
+            op = ops.pop()
+            b = nums.pop()
+            a = nums.pop()
+            nums.append(apply_op(op, b, a))
+        return nums[0] 
