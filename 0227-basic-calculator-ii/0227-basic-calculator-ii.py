2c0d81b3a56c1ccd1dddfb54be787663f3757b31
Tee Mak <9258270+teemak@users.noreply.github.com>
2025-05-04 15:10:52 -0400

    Time: 78 ms (43.08%), Space: 18.9 MB (92.3%) - LeetHub

--- 0227-basic-calculator-ii/0227-basic-calculator-ii.py
+++ 0227-basic-calculator-ii/0227-basic-calculator-ii.py
@@ -1,32 +1,34 @@
 class Solution:
     def calculate(self, s: str) -> int:
+        s = s.replace(' ', '')
         n = len(s)
+        result = tail = num = i = 0
         operator = '+'
-        num = result = tail = i = 0
 
         while i < n:
-            element = s[i]
-            if element.isdigit():
+            ch = s[i]
+
+            if ch.isdigit():
                 num = 0
                 while i < n and s[i].isdigit():
                     num = num * 10 + int(s[i])
                     i += 1
-                match operator:
-                    case '+':
-                        result += num
-                        tail = num
-                    case '-':
-                        result -= num
-                        tail = -num
-                    case '*':
-                        result = result - tail + (tail * num)
-                        tail = tail * num
-                    case '/':
-                        result = result - tail + int(tail / num)
-                        tail = int(tail / num)
-                num = 0
+                if operator == '+':
+                    result += num
+                    tail = num
+                elif operator == '-':
+                    result -= num
+                    tail = -num
+                elif operator == '*':
+                    result = result - tail + (tail * num)
+                    tail = tail * num
+                elif operator == '/':
+                    result = result - tail + int(tail / num)
+                    tail = int(tail / num)
                 continue
-            elif element in '+-*/':
-                operator = element
+            elif ch in '+-*/':
+                operator = ch
+            
             i += 1
         return result
+                
