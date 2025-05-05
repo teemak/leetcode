91af39b63a9aafe777110f6125d9f8c485462255
Tee Mak <9258270+teemak@users.noreply.github.com>
2025-05-04 20:24:31 -0400

    Time: 1 ms (66.81%), Space: 18 MB (40.13%) - LeetHub

--- 0806-domino-and-tromino-tiling/0806-domino-and-tromino-tiling.py
+++ 0806-domino-and-tromino-tiling/0806-domino-and-tromino-tiling.py
@@ -5,13 +5,13 @@ class Solution:
             return 1
         if n == 2:
             return 2
-        if n == 3:
-            return 5
+        if n == 0:
+            return 0
         
-        dp = [0] * (n + 1)
-        dp[0], dp[1], dp[2], dp[3] = 1, 1, 2, 5
+        dp = [1,1,2]
 
-        for i in range(4, n + 1):
-            dp[i] = (2 * dp[i - 1] + dp[i - 3]) % mod
+        for i in range(3, n + 1):
+            val = (dp[i - 3] + 2 * dp[i - 1]) % mod
+            dp.append(val)
 
-        return dp[n]
+        return dp[-1]
