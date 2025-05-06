2914e882fcbc36a4478d416a4c9cec3fbf003a4b
Tee Mak <9258270+teemak@users.noreply.github.com>
2025-05-06 11:24:12 -0400

    Time: 3617 ms (69.29%), Space: 17.9 MB (39.89%) - LeetHub

--- 0079-word-search/0079-word-search.py
+++ 0079-word-search/0079-word-search.py
@@ -1,39 +1,29 @@
 class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
-        if not board or not word:
-            return False
-
         rows, cols = len(board), len(board[0])
-        n = len(word)
-        if n > rows * cols:
-            return False
-        cell_counts = Counter(cell for row in board for cell in row)
-        char_counts = Counter(word)
-        for char, count in char_counts.items():
-            if cell_counts[char] < count:
-                return False
-        
-        if char_counts[word[-1]] < char_counts[word[0]]:
-            word = word[::-1]
-        
         directions = [(1,0), (-1,0), (0,1), (0,-1)]
 
-        def dfs(row, col, index, visited):
-            if index == n:
+        def dfs(r, c, i):
+            #base case
+            if i == len(word):
                 return True
-            if row < 0 or row >= rows or col < 0 or col >= cols or (row, col) in visited or board[row][col] != word[index]:
-                return False
-            visited.add((row, col))
+            if r < 0 or r >= rows or c < 0 or c >= cols or board[r][c] != word[i]:
+                return
+
+            temp = board[r][c]
+            board[r][c] = '#'
+
+            #recursive case
             for dx, dy in directions:
-                x, y = dx + row, dy + col
-                if dfs(x, y, index + 1, visited):
+                if dfs(r + dx, c + dy, i + 1):
                     return True
-            visited.remove((row, col))
+
+            board[r][c] = temp
             return False
+
 
         for row in range(rows):
             for col in range(cols):
-                if dfs(row, col, 0, set()):
+                if dfs(row,col,0):
                     return True
-        
         return False
