fe148b7a68ea64c3cee1d4f40f733d9598edae45
Tee Mak <9258270+teemak@users.noreply.github.com>
2025-05-06 12:03:54 -0400

    Time: 0 ms (100%), Space: 18.7 MB (73.71%) - LeetHub

--- 0792-binary-search/0792-binary-search.py
+++ 0792-binary-search/0792-binary-search.py
@@ -11,7 +11,7 @@ class Solution:
                 return mid
             elif nums[mid] < target:
                 left = mid + 1
-            elif nums[mid] > target:
+            else:
                 right = mid - 1
 
         return -1
