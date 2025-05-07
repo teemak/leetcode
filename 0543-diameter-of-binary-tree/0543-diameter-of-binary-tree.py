d147f710d6a998c136c3f4672ba835ac94c33ff1
Tee Mak <9258270+teemak@users.noreply.github.com>
2025-05-07 04:23:37 -0400

    Time: 4 ms (73.82%), Space: 20.8 MB (48.63%) - LeetHub

--- 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.py
+++ 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.py
@@ -12,12 +12,10 @@ class Solution:
             nonlocal diameter
             if not node:
                 return 0
-
             left = dfs(node.left)
             right = dfs(node.right)
             diameter = max(diameter, left + right)
-
             return max(left, right) + 1
-
+        
         dfs(root)
         return diameter
