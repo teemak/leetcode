baed7eebea0bf40f226f66354cfaae5a21797d9a
Tee Mak <9258270+teemak@users.noreply.github.com>
2025-05-08 19:32:10 -0400

    Time: 11 ms (58.58%), Space: 18 MB (40.3%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.py
+++ 0039-combination-sum/0039-combination-sum.py
@@ -1,19 +1,24 @@
+'''
+2                 running_count     logic
+2+2+2             6                 keep iterating 
+2+2+2+2           8 
+
+
+running_count
+
+'''
 class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
         result = []
-
         def dfs(start, path, total):
-            if target == total:
+            if total == target:
                 result.append(path[:])
-                return
+                return 
             if total > target:
                 return
-            
             for i in range(start, len(candidates)):
-                num = candidates[i]
-                path.append(num)
-                dfs(i, path, total + num)
+                path.append(candidates[i])
+                dfs(i, path, total + candidates[i])
                 path.pop()
-            
         dfs(0, [], 0)
         return result
