28955b3ce1c5da8a7393a9e4833f0296e7c8b5a3
Tee Mak <9258270+teemak@users.noreply.github.com>
2025-05-11 10:22:19 -0400

    Time: 2 ms (39.45%), Space: 18 MB (57.17%) - LeetHub

--- 0046-permutations/0046-permutations.py
+++ 0046-permutations/0046-permutations.py
@@ -1,5 +1,6 @@
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
+        ans = []
         def backtrack(curr):
             if len(curr) == len(nums):
                 ans.append(curr[:])
@@ -9,7 +10,5 @@ class Solution:
                     curr.append(num)
                     backtrack(curr)
                     curr.pop()
-
-        ans = []
         backtrack([])
         return ans
