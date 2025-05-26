7b958df4f2d1f78ec6aa0e3c77c16f2d7a4cae11
Tee Mak <9258270+teemak@users.noreply.github.com>
2025-05-26 11:37:14 -0400

    Time: 0 ms (100%), Space: 18.3 MB (13.02%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.py
+++ 0733-flood-fill/0733-flood-fill.py
@@ -3,14 +3,14 @@ class Solution:
         if image[sr][sc] == color:
             return image
         rows, cols = len(image), len(image[0])
-        directions = [(1,0), (-1,0), (0,1), (0,-1)]
+        directions = [(1, 0), (-1, 0), (0, 1), (0, -1)]
         original = image[sr][sc]
-         
+
         def dfs(r, c):
             if r < 0 or r >= rows or c < 0 or c >= cols or image[r][c] != original:
                 return 
             image[r][c] = color
             for dx, dy in directions:
-                dfs(dx + r, dy + c)  
+                dfs(dx + r,  dy + c)
         dfs(sr, sc)
         return image
